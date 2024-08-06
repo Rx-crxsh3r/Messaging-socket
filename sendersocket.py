6f70cfc72126fd3d1991bff8cc50d571d6733932
Rx-crxsh3r <ahmed.ab2824@gmail.com>
2024-08-06 08:21:55 +0800

    Update sendersocket.py

--- sendersocket.py
+++ sendersocket.py
@@ -1,32 +1,40 @@
-#import the socket library 
-import socket             
- 
-# next create a socket object 
-s = socket.socket()         
-print ("Socket successfully created")
- 
-# reserve a port on your computer 
-port = 12345               
- 
-# Next bind to the port 
-s.bind(('', port))         
-print ("socket binded to %s" %(port)) 
- 
-# put the socket into listening mode 
-s.listen(5)     
-print ("socket is listening")            
- 
-# a forever loop until we interrupt it or an error occurs 
-while True: 
-# Establish connection with client. 
-  c, addr = s.accept()     
-  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type. 
-  c.send('Thank you for connecting'.encode()) 
- 
-  # Close the connection with the client 
-  c.close()
-   
-  # Breaking once connection closed
-  break
+import socket
+
+def server_program():
+    # create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # get the local machine name
+    host = socket.gethostname()
+    port = 12345
+    
+    # bind the socket to the port
+    server_socket.bind((host, port))
+    
+    # configure how many clients the server can listen simultaneously
+    server_socket.listen(2)
+    print("Server is listening...")
+    
+    # accept a connection
+    conn, address = server_socket.accept()
+    print(f"Connection from {address} has been established!")
+    
+    while True:
+        # receive data from the client
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print(f"Received from client: {data}")
+        
+        # send data to the client
+        message = input(" -> ")
+        conn.send(message.encode())
+        
+        if message.lower() == 'bye':
+            break
+    
+    conn.close()
+    server_socket.close()
+
+if __name__ == '__main__':
+    server_program()
