9cada89034396e5b848b6ea3f5d616359097ee7a
Rx-crxsh3r <ahmed.ab2824@gmail.com>
2024-08-06 08:21:14 +0800

    added the functionality of sending messages, one user at a time

--- recieversocket.py
+++ recieversocket.py
@@ -1,16 +1,32 @@
-# Import socket module 
-import socket             
+import socket
 
-# Create a socket object 
-s = socket.socket()
+def client_program():
+    # create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # get the local machine name
+    host = socket.gethostname()
+    port = 12345
+    
+    # connect to the server
+    client_socket.connect((host, port))
+    
+    while True:
+        # send data to the server
+        message = input(" -> ")
+        client_socket.send(message.encode())
+        
+        if message.lower() == 'bye':
+            break
+        
+        # receive data from the server
+        data = client_socket.recv(1024).decode()
+        print(f"Received from server: {data}")
+        
+        if data.lower() == 'bye':
+            break
+    
+    client_socket.close()
 
-# Define the port on which you want to connect 
-port = 12345
-
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port))
-
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection 
-s.close()
+if __name__ == '__main__':
+    client_program()
